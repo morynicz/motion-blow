bfee2b43727bc9d0f2b60b60837804b8da37b57a
Michał Orynicz <morynicz@gmail.com>
2024-03-02 19:51:38 +0100

    Introduce thread safe Queue

---
+++ code/include/motion_blow/Queue.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <mutex>
+#include <optional>
+#include <deque>
+
+template<typename T>
+class Queue
+{
+    public:
+    Queue(uint maxSize):maxSize(maxSize){};
+    void push(const T& item)
+    {
+        std::lock_guard<std::mutex> guard(mtx);
+        buffer.push_front(item);
+        if(buffer.size() > maxSize)
+            buffer.pop_back();
+    }
+    std::optional<T> pop()
+    {
+        std::lock_guard<std::mutex> guard(mtx);
+        if(not buffer.empty())
+        {
+            auto it = buffer.back();
+            buffer.pop_back();
+            return it;
+        }
+        return std::nullopt;
+    }
+    private:
+    uint maxSize;
+    std::mutex mtx{};
+    std::deque<T> buffer{};
+};
---
+++ code/src/QueueTests.cpp
@@ -0,0 +1,54 @@
+#include "motion_blow/Queue.hpp"
+#include "gtest/gtest.h"
+
+struct QueueTests: public testing::Test
+{
+    Queue<int> queue{3u};
+};
+
+TEST_F(QueueTests, emptyQueueReturnsNullopt)
+{
+    EXPECT_EQ(queue.pop(), std::nullopt);
+}
+
+TEST_F(QueueTests, elementPushedIntoQueueCanBeRetrieved)
+{
+    const int pushedIn{1234};
+    queue.push(pushedIn);
+
+    EXPECT_EQ(queue.pop(), pushedIn);
+}
+
+TEST_F(QueueTests, queueIsAFifo)
+{
+    const int el1{1234};
+    const int el2{5678};
+    queue.push(el1);
+    queue.push(el2);
+
+    EXPECT_EQ(queue.pop(), el1);
+    EXPECT_EQ(queue.pop(), el2);
+}
+
+TEST_F(QueueTests, queueIsEmptyAfterAllElementsPopped)
+{
+    const int el1{9090};
+    queue.push(el1);
+    queue.pop();
+    
+    EXPECT_EQ(queue.pop(), std::nullopt);
+}
+
+TEST_F(QueueTests, queueDropsOldestElementWhenOverLimit)
+{
+    const int el1{1234};
+    const int el2{5678};
+    const int el3{9012};
+    const int el4{3456};
+    queue.push(el1);
+    queue.push(el2);
+    queue.push(el3);
+    queue.push(el4);
+
+    EXPECT_EQ(queue.pop(), el2);
+}
